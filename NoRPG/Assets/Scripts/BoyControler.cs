02477fa1376ad8359f820005f24dd99e1458355a
Mehmet Ali Incekara <xnappy@web.de>
2016-11-25 21:23:06 +0100

    Add Forest Dungeon

---
+++ NoRPG/Assets/Scripts/BoyControler.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float rotateVel = 100;
+        public float forwardVel = 12;
+        public float jumpVel = 25;
+        public float distToGrounded = 0.1f;
+        public LayerMask ground;
+    }
+
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downAccel = 0.75f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
+
+    public MoveSettings moveSettings = new MoveSettings();
+    public PhysSettings physSettings = new PhysSettings();
+    public InputSettings inputSettings = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput, jumpInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSettings.distToGrounded, moveSettings.ground);
+    }
+	
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The character needs a rigidbody.");
+
+        forwardInput = turnInput = jumpInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS); //interpolated
+        turnInput = Input.GetAxis(inputSettings.TURN_AXIS); //interpolated
+        jumpInput = Input.GetAxis(inputSettings.JUMP_AXIS); //non-interpolated
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+
+        rBody.velocity = transform.TransformDirection(velocity);
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputSettings.inputDelay)
+        {
+            //move
+            velocity.z = moveSettings.forwardVel * forwardInput;
+        }
+        else
+            // zero velocity
+            velocity.z = 0;
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputSettings.inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSettings.rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+
+        transform.rotation = targetRotation;
+    }
+
+    void Jump()
+    {
+        if (jumpInput > 0 && Grounded())
+        {
+            //jump
+            velocity.y = moveSettings.jumpVel;
+        }
+        else if (jumpInput == 0 && Grounded())
+        {
+            //zero out our velocity.y
+            velocity.y = 0;
+        }
+        else
+        {
+            //decrease velocity.y
+            velocity.y -= physSettings.downAccel;
+        }
+    }
+}
