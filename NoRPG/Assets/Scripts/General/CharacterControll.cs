b936a3a17a8462ba58075ffdd97e793dd7db8f61
Tom433 <tom@wolske.tv>
2016-11-28 17:46:25 +0100

    Add Lavawelt

---
+++ NoRPG/Assets/Scripts/General/CharacterControll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControll : MonoBehaviour {
+
+    private CharacterController character;
+    // Use this for initialization
+    void Start () {
+        character = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        character.SimpleMove(new Vector3(1, 0, 0));
+
+    }
+}
