ba467ee2c18ed7aca95642584715a7226f66fb39
Tom433 <tom@wolske.tv>
2016-12-02 17:03:20 +0100

    Update Lavawelt

--- NoRPG/Assets/Scripts/General/CharacterControll.cs
+++ NoRPG/Assets/Scripts/General/CharacterControll.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class CharacterControll : MonoBehaviour {
 
     private CharacterController character;
+    public int x = 0;
+    public int z = 0;
+
     // Use this for initialization
     void Start () {
         character = GetComponent<CharacterController>();
@@ -11,7 +14,7 @@ public class CharacterControll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        character.SimpleMove(new Vector3(1, 0, 0));
+        character.SimpleMove(new Vector3(x, 0, z));
 
     }
 }
