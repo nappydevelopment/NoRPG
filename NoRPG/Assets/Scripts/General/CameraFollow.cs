1986e0e1db50a7662249022e34deec0eece196c4
xnappy <xnappy@web.de>
2017-02-01 10:34:42 +0100

    Add Player to forest. Add Minimap to HUD

---
+++ NoRPG/Assets/Scripts/General/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform Target;
+	
+	void LateUpdate () {
+		transform.position = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+		
+	}
+}
