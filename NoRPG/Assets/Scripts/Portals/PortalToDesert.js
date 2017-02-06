4960f33eae52f2620f7afcc19c981888177282ef
xnappy <xnappy@web.de>
2017-02-06 16:32:50 +0100

    Update Portalsystem

--- NoRPG/Assets/Scripts/Portals/PortalToDesert.js
+++ NoRPG/Assets/Scripts/Portals/PortalToDesert.js
@@ -4,6 +4,7 @@ function OnTriggerEnter(Col : Collider)
     {
         if (Col.tag == "Player")
         {
+            DontDestroyOnLoad(Col.gameObject);
             Application.LoadLevel("Scenes/Desert");
             //SceneManager.LoadScene("Scenes/Desert");
         }
