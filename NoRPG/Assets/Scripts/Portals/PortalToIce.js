d120274445a49f04ff5313fb5e466717223606e9
xnappy <xnappy@web.de>
2017-02-09 11:25:34 +0100

    Add SnowWorld empty scene and edit Settings

--- NoRPG/Assets/Scripts/Portals/PortalToIce.js
+++ NoRPG/Assets/Scripts/Portals/PortalToIce.js
@@ -1,9 +1,10 @@
 ﻿#pragma strict
 
-function Start () {
-	
-}
-
-function Update () {
-	
-}
+function OnTriggerEnter(Col : Collider)
+    {
+        if (Col.tag == "Player")
+        {
+            DontDestroyOnLoad(Col.gameObject);
+            Application.LoadLevel("Scenes/Snow_World"); 
+        }
+    }
