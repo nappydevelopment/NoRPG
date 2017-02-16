2dbb68789700d0e5cd4ca3eeb74c512d9fd385c5
Mehmet Ali Incekara <xnappy@web.de>
2017-02-16 10:37:55 +0100

    Finish Forest and start interaction

---
+++ NoRPG/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetButtonDown("InteractButton")) && !UnityEngine.EventSystems.EventTrigger.Entry.)
+                GetInteraction();
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted")
+            }
+            else
+            {
+
+            }
+        }
+    }
+	
+}
