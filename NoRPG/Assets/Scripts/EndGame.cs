04fc1d93c2ea8699763cf24c8bc23277f1157433
Mehmet Ali Incekara <xnappy@web.de>
2017-02-28 18:12:10 +0100

    Update Startscreen and HUD

---
+++ NoRPG/Assets/Scripts/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour {
+
+    public void EndGameButton()
+    {
+        SceneManager.LoadScene("Scenes/Startscreen", LoadSceneMode.Single);
+    }
+}
---
+++ NoRPG/Assets/Scripts/Startscreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startscreen : MonoBehaviour {
+
+    public GameObject settingsCanvas;
+
+	void Update () {
+        HideIfClickedOutside(settingsCanvas);
+    }
+
+    private void HideIfClickedOutside(GameObject panel)
+    {
+        if (Input.anyKeyDown && !RectTransformUtility.RectangleContainsScreenPoint(panel.GetComponent<RectTransform>(),Input.mousePosition,Camera.main))
+        {
+            panel.SetActive(false);
+        }
+    }
+}
