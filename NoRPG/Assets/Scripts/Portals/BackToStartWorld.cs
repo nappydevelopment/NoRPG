773f737457d98f7252ca1a30fa0fbac8ae96765d
Mehmet Ali Incekara <xnappy@web.de>
2017-03-12 15:51:34 +0100

    Add LoadingScreen

--- NoRPG/Assets/Scripts/Portals/BackToStartWorld.cs
+++ NoRPG/Assets/Scripts/Portals/BackToStartWorld.cs
@@ -1,13 +1,48 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class BackToStartWorld : MonoBehaviour {
+
+    AsyncOperation ao;
+
+    public Canvas hud;
+    public Canvas loadingScreen;
+    public Slider progBar;
 
     void OnTriggerEnter()
     {
         string currentScene = SceneManager.GetActiveScene().name;
         PortalControl.control.cameFrom = currentScene;
 
-        SceneManager.LoadScene("Scenes/Startwelt", LoadSceneMode.Single);
+        hud.gameObject.SetActive(false);
+        loadingScreen.gameObject.SetActive(true);
+
+        StartCoroutine(LoadLevelWithRealProgress());
+    }
+
+    IEnumerator LoadLevelWithRealProgress()
+    {
+        yield return new WaitForSeconds(1);
+
+        ao = SceneManager.LoadSceneAsync("Scenes/Startwelt", LoadSceneMode.Single);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            progBar.value = ao.progress;
+
+            if (ao.progress == 0.9f)
+            {
+                progBar.value = 1f;
+                ao.allowSceneActivation = true;
+            }
+
+            Debug.Log(ao.progress);
+            yield return null;
+        }
+
     }
 }
--- NoRPG/Assets/Scripts/Portals/PortalToForrest.cs
+++ NoRPG/Assets/Scripts/Portals/PortalToForrest.cs
@@ -1,11 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PortalToForrest : MonoBehaviour
 {
+    AsyncOperation ao;
+
+    public Canvas hud;
+    public Canvas loadingScreen;
+    public Slider progBar;
+
     void OnTriggerEnter()
     {
-        DontDestroyOnLoad(gameObject);
-        SceneManager.LoadScene("Scenes/first_forrest", LoadSceneMode.Single);
+        
+        loadingScreen.gameObject.SetActive(true);
+
+        StartCoroutine(LoadLevelWithRealProgress());
+
+        hud.gameObject.SetActive(true);
+    }
+
+    IEnumerator LoadLevelWithRealProgress()
+    {
+        yield return new WaitForSeconds(1);
+
+        ao = SceneManager.LoadSceneAsync("Scenes/first_forrest", LoadSceneMode.Single);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            progBar.value = ao.progress;
+
+            if (ao.progress == 0.9f)
+            {
+                progBar.value = 1f;
+                ao.allowSceneActivation = true;
+            }
+
+            Debug.Log(ao.progress);
+            yield return null;
+        }
     }
 }
