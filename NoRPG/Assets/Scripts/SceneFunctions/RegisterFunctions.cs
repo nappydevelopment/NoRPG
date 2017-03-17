4f8eb770f0d9d93a40190d56bf404d06705f7e02
Mehmet Ali Incekara <xnappy@web.de>
2017-03-17 12:43:23 +0100

    Add new Player and Mao to Desert, Lavawelt, Forest and Startwelt

--- NoRPG/Assets/Scripts/SceneFunctions/RegisterFunctions.cs
+++ NoRPG/Assets/Scripts/SceneFunctions/RegisterFunctions.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RegisterFunctions : MonoBehaviour {
+public class RegisterFunctions : MonoBehaviour
+{
 
     public void StartAnimationOnClick(Animation animation)
     {
-        animation.Play("Cheer");
-        WaitForAnimation(animation);
+        StartCoroutine(WaitForAnimation(animation));
     }
 
     public IEnumerator WaitForAnimation(Animation animation)
     {
-        do
+        animation.Play("Cheer");
+
+        while (animation.isPlaying)
         {
             yield return null;
-        } while (animation.isPlaying);
-
-        yield return animation;
+        }
     }
-
 }
