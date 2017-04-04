b80e619e3ed49012db15c448743b4f1878bc52ac
Mehmet Ali Incekara <xnappy@web.de>
2017-04-04 11:25:18 +0200

    Fix npc dialogue

--- NoRPG/Assets/Scripts/NPC/NPCCommunication.cs
+++ NoRPG/Assets/Scripts/NPC/NPCCommunication.cs
@@ -9,7 +9,7 @@ public class NPCCommunication : MonoBehaviour {
     public FindNearestHandler trader;
     public GameObject player;
 
-    public GameObject commObjekt;
+    public GameObject textBox;
     public Image npcImage;
     public Text npcName;
     public Text npcText;
@@ -21,21 +21,19 @@ public class NPCCommunication : MonoBehaviour {
 
     public Button acceptButton;
     public Button cancelButton;
-
     public Button openMenuButton;
     public Button openMapButton;
     
     public GameObject descriptionPanel;
     public Transform gamelistPanel;
     public List<Game> gameList;
+
     public GameScrollList scrollList = new GameScrollList();
+    NPCDialogue dialogue = new NPCDialogue();
 
     private List<Games> games;
     private bool nextPageFlag;
     public ScriptedStartAnimation ssa;
-
-    NPCDialogue dialogue = new NPCDialogue();
-    //GameScrollList scrollList = new GameScrollList();
 
     [SerializeField]
     private float distance = 3.0f;
@@ -45,6 +43,8 @@ public class NPCCommunication : MonoBehaviour {
         // If interactable Object is near to the player then start the interaction, else do nothing
         if (Vector3.Distance(player.transform.position, trader.GetClosestObject("InteractionObject", player).transform.position) < distance)
         {
+            acceptButton.onClick.RemoveAllListeners();
+            cancelButton.onClick.RemoveAllListeners();
             string interactableObjectName = trader.closest.name;
             string npcType = dialogue.GetNpcType(interactableObjectName);
 
@@ -53,7 +53,7 @@ public class NPCCommunication : MonoBehaviour {
             openMenuButton.interactable = false;
 
             //set speech field active
-            commObjekt.SetActive(true);
+            textBox.SetActive(true);
 
             //get content for communication
             npcName.text = dialogue.GetNpcName(interactableObjectName);
@@ -61,23 +61,26 @@ public class NPCCommunication : MonoBehaviour {
             Char delimiter = '%';
             string npcCompleteText = dialogue.GetNpcText(interactableObjectName);
             string[] npcTextArray = npcCompleteText.Split(delimiter);
-            Debug.Log("Arraylength= " + npcTextArray.Length);
 
             //if there is more then one page for this communication 
             if (npcTextArray.Length > 1)
             {
-                Debug.Log("Is in IF, length > 1");
-                //StartCoroutine(GoThroughConversation(npcTextArray));
-                GoThroughConv(npcTextArray);
-                Debug.Log("Verlasse gothroughconversation");
+                Debug.Log("ARRAY LENGTH > 1");
+                StartCoroutine(GoThroughConversation(npcTextArray));
+            }
+            else
+            {
+                Debug.Log("ARRAY LENGTH = 1");
+                npcText.text = dialogue.GetNpcText(interactableObjectName);
             }
 
+            Debug.Log("GET CORRECT NPC TYPE");
             if (npcType == "Trader")
             {
                 acceptButton.onClick.AddListener(delegate () { OpenGameList(interactableObjectName); });
                 cancelButton.onClick.AddListener(delegate () { CancelCommunication(); });
             }
-            else if( npcType == "ScriptedNPC")
+            else if (npcType == "ScriptedNPC")
             {
                 if (interactableObjectName == "Buergermeister")
                 {
@@ -101,26 +104,23 @@ public class NPCCommunication : MonoBehaviour {
 
     private IEnumerator GoThroughConversation(string[] textArray)
     {
-        Debug.Log("GoThroughConversation");
-        foreach (var i in textArray)
+        for (int i = 0; i < textArray.Length; i++)
         {
             nextPageFlag = false;
-            Debug.Log(i.ToString());
-            npcText.text = i.ToString();
-            acceptButton.onClick.AddListener(() => nextPageFlag = true);
-            yield return new WaitUntil(() => nextPageFlag == true);
-            //yield return null;
-        }
-    }
 
-    private void GoThroughConv(string[] npcTextArray)
-    {
-        foreach (var i in npcTextArray)
-        {
-            nextPageFlag = false;
-            npcText.text = i.ToString();
-            acceptButton.onClick.AddListener(() => nextPageFlag = true);
+            Debug.Log(textArray[i]);
+            npcText.text = textArray[i];
 
+            acceptButton.onClick.RemoveAllListeners();
+            cancelButton.onClick.RemoveAllListeners();
+            acceptButton.onClick.AddListener(() => nextPageFlag = true);
+            cancelButton.onClick.AddListener(() => nextPageFlag = true);
+
+            while (nextPageFlag == false)
+            {
+                Debug.Log("Waiting for input!");
+                yield return null;
+            }
         }
     }
 
@@ -138,20 +138,18 @@ public class NPCCommunication : MonoBehaviour {
 
     private void OpenGameList(string interactableObjectName)
     {
-        commObjekt.SetActive(false);
+        textBox.SetActive(false);
         gamelistObject.SetActive(true);
 
         FillPanelWithGames();
 
         gamelistTitle.text = dialogue.GetGamelistTitle(interactableObjectName);
         gamelistDescription.text = dialogue.GetGamelistDescription(interactableObjectName);
-
     }
 
     private void FillPanelWithGames()
     {
         Game newGame1 = new Game();
-        //get GameName and Downloadlink from GetGameList()
         newGame1.gameName = "Nappy The Ingenious";
         newGame1.downloadLink = "http://www.google.de";
         gameList.Add(newGame1);
@@ -165,12 +163,13 @@ public class NPCCommunication : MonoBehaviour {
         descriptionPanel.SetActive(false);
         gamelistPanel.gameObject.SetActive(true);
 
-        //a message like "Thank you for visiting the shop!"
         npcText.gameObject.SetActive(false);
         npcFarewellText.gameObject.SetActive(true);
 
-        commObjekt.SetActive(true);
+        textBox.SetActive(true);
 
+        acceptButton.onClick.RemoveAllListeners();
+        cancelButton.onClick.RemoveAllListeners();
         acceptButton.onClick.AddListener(delegate () { CancelCommunication(); });
         cancelButton.onClick.AddListener(delegate () { CancelCommunication(); });
     }
@@ -180,7 +179,7 @@ public class NPCCommunication : MonoBehaviour {
         npcText.gameObject.SetActive(true);
         npcFarewellText.gameObject.SetActive(false);
 
-        commObjekt.SetActive(false);
+        textBox.SetActive(false);
         gamelistObject.SetActive(false);
 
         openMapButton.interactable = true;
