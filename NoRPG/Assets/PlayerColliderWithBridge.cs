ba09d5afe20f227707f1c55ddc55630ee2884c25
Tom433 <tom@wolske.tv>
2017-04-09 19:29:58 +0200

    Change ShipPassengers
    Now the Player goes with the ship and don't falls down
    also the Dialog is not showing after the drive is end

---
+++ NoRPG/Assets/PlayerColliderWithBridge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColliderWithBridge : MonoBehaviour {
+
+    public GameObject player;
+    public ShipPassenger sp;
+
+    void OnCollisionEnter (Collision col) {
+        if (col.gameObject.name == player.name) {
+            Debug.Log("Player On Bridge");
+            sp.setShippedStatusToFalse();
+        }
+    }
+}
